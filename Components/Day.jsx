a62f35a9ce2c60e5192b974645788f788b79eb34
Shreyasshukla957 <shuklashreyas.shs@gmail.com>
2026-01-26 15:36:14 +0530

    Updated features Time & Day

--- Components/Day.jsx
+++ Components/Day.jsx
@@ -39,17 +39,16 @@ export default function Day() {
       dragConstraints={{ left: 0, right: 1000, top: 0, bottom: 500 }}
       whileDrag={{ scale: 1.05 }}
       whileHover={{ cursor: "grabbing", scale: 1.05 }}
-      dragElastic={1}
       transition={{
         type: "spring",
-        bounce: 1,
+
         duration: 4,
         ease: "easeInOut",
         translateX: "-30%",
       }}
       className={`from-zinc-90/60 bg-opacity-80 font-techmono pb-2" z-20 hidden h-20 w-21 flex-col items-center overflow-hidden rounded-full border border-l-0 border-dashed px-4 pt-5 backdrop-blur-[100%] select-none selection:bg-slate-50/70 lg:flex ${
         throwball
-          ? "border border-neutral-400 bg-neutral-200/40 from-neutral-600 to-neutral-500 shadow-neutral-400/20"
+          ? "border-none bg-linear-to-b from-orange-600 via-orange-600 to-orange-400 shadow-2xl"
           : "border-y-neutral-100/9 border-r-neutral-100/25 bg-linear-to-b to-zinc-900/80 pl-3 font-extralight shadow-[0px_5px_20px_rgba(0,0,0,0.2)]"
       }`}
       onHoverStart={() => setthrowball(!throwball)}
